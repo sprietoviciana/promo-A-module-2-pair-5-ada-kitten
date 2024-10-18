4e40c9daa4b6104071c9b538d7b36f56934be605
Silvia Prieto <info@silviaprieto.com>
2024-10-18 11:23:07 +0200

    functions

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const list = document.querySelector(".js-list");
 
 const kittenImage1 = "https://dev.adalab.es/gato-siames.webp";
 const kittenName1 = "Anastacio";
-const kittenDesc1 = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente. "
-const kittenBreed1 = "Siamés"
-const kittenOne =
- `<li class="card">
+const kittenDesc1 =
+  "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente. ";
+const kittenBreed1 = "Siamés";
+const kittenOne = `<li class="card">
   <article>
   <img
     class="card_img"
@@ -23,8 +23,9 @@ const kittenOne =
 
 const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp";
 const kittenName2 = "Fiona";
-const kittenDesc2 = " Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo."
-const kittenBreed2 = "Sphynx"
+const kittenDesc2 =
+  " Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+const kittenBreed2 = "Sphynx";
 const kittenTwo = `<li class="card">
   <img
     class="card_img"
@@ -38,11 +39,11 @@ const kittenTwo = `<li class="card">
   </p>
 </li>`;
 
-
 const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
 const kittenName3 = "Cielo";
-const kittenDesc3 = " Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta."
-const kittenBreed3 = "Maine Coon"
+const kittenDesc3 =
+  " Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+const kittenBreed3 = "Maine Coon";
 
 const kittenThree = `<li class="card">
   <img
@@ -62,23 +63,30 @@ list.innerHTML = kittenOne + kittenTwo + kittenThree;
 const addCat = document.querySelector(".js-btn-add");
 const newForm = document.querySelector(".new-form");
 
-addCat.addEventListener("click", () => {
-  newForm.classList.remove("collapsed");
-  console.log("la usuaria hace click");
-});
+
 
 const buttonsearch = document.querySelector(".js_button-search");
 const input = document.querySelector(".js_in_search_desc");
 
 if (input.includes(kittenDesc1)) {
-    buttonsearch.addEventListener("click", (event) => {
-  event.preventDefault();
-  const descriptionInput = input.value;
-  list.innerHTML = kittenOne;
- 
-  console.log("gatito")
-})
-else {
+  buttonsearch.addEventListener("click", (event) => {
+    event.preventDefault();
+    const descriptionInput = input.value;
+    list.innerHTML = kittenOne;
+  });
   list.innerHTML = kittenOne + kittenTwo + kittenThree;
- }
 }
+
+function showNewCatForm() {
+  newForm.classList.remove("collapsed");
+  addCat.addEventListener("click", () => {
+
+  });
+}
+
+function hideNewCatForm() {
+  newForm.classList.add("collapsed");
+}
+
+buttonsearch.addEventListener("click", showNewCatForm);
+console.log("showNewCatForm");
